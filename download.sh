72447ff416bbc22ae34010db12eb745ae67976f7
Taryx <taryx@proton.me>
2025-02-05 15:46:53 +0800

    fix(deps): downgrade agent to 1.2.0

--- download.sh
+++ download.sh
@@ -7,7 +7,8 @@ tar -xz -f AdGuardHome_linux_amd64.tar.gz && rm AdGuardHome_linux_amd64.tar.gz
 mv ./AdGuardHome/AdGuardHome ./AdGuard && chmod +x ./AdGuard
 rm -r ./AdGuardHome
 
-AGENT_VERSION="$(curl -s https://api.github.com/repos/nezhahq/agent/releases | grep -m 1 -oP '"tag_name":\s*"\K[^"]+')"
+#AGENT_VERSION="$(curl -s https://api.github.com/repos/nezhahq/agent/releases | grep -m 1 -oP '"tag_name":\s*"\K[^"]+')"
+AGENT_VERSION="v1.2.0"
 
 wget -q https://github.com/nezhahq/agent/releases/download/${AGENT_VERSION}/nezha-agent_linux_amd64.zip
 unzip nezha-agent_linux_amd64.zip && rm nezha-agent_linux_amd64.zip
